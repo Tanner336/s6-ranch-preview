5f1a77fd9cd988e470ae20908d49415ceba7e5c0
Tanner Cook <tannercook336@gmail.com>
2024-02-18 19:21:13 -0500

    chore: added fractal tag to line 102

--- src/routes/27-horses-for-sale.tsx
+++ src/routes/27-horses-for-sale.tsx
@@ -1,120 +1,123 @@
 import { Fragment, useRef } from "react";
 import { Swiper, SwiperRef, SwiperSlide, useSwiper } from "swiper/react";
-import { Navigation, EffectCoverflow } from 'swiper/modules';
-import 'swiper/css';
+import { Navigation, EffectCoverflow } from "swiper/modules";
+import "swiper/css";
 
 import img1 from "../assets/img/maggie-horse.jpg";
 import img2 from "../assets/img/maggie-horse.jpg";
 import img3 from "../assets/img/maggie-horse.jpg";
 
 type CardDataItem = {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 };
 
 type CardInfo = {
-    img: string;
-    cardDataItems: CardDataItem[];
-    last: string;
-    lastUrl: string;
-
+  img: string;
+  cardDataItems: CardDataItem[];
+  last: string;
+  lastUrl: string;
 };
 
 const cardDataItems: CardDataItem[] = [
-    {
-        title: "Name",
-        description: "Seabiscuit",
-    },
-    {
-        title: "Breed",
-        description: "Male",
-    },
-    {
-        title: "Price",
-        description: "10K",
-    },
+  {
+    title: "Name",
+    description: "Seabiscuit",
+  },
+  {
+    title: "Breed",
+    description: "Male",
+  },
+  {
+    title: "Price",
+    description: "10K",
+  },
 ];
 
 const cardInfos: CardInfo[] = [
-    {
-        img: img1,
-        cardDataItems: cardDataItems,
-        last: 'Buy Horse now yada yada yada',
-        lastUrl: './voting.html'
-    },
-    {
-        img: img2,
-        cardDataItems: cardDataItems,
-        last: 'Buy Horse now yada yada yada',
-        lastUrl: './voting.html'
-    },
-    {
-        img: img3,
-        cardDataItems: cardDataItems,
-        last: 'Buy Horse now yada yada yada',
-        lastUrl: './voting.html'
-    },
+  {
+    img: img1,
+    cardDataItems: cardDataItems,
+    last: "Buy Horse now yada yada yada",
+    lastUrl: "./voting.html",
+  },
+  {
+    img: img2,
+    cardDataItems: cardDataItems,
+    last: "Buy Horse now yada yada yada",
+    lastUrl: "./voting.html",
+  },
+  {
+    img: img3,
+    cardDataItems: cardDataItems,
+    last: "Buy Horse now yada yada yada",
+    lastUrl: "./voting.html",
+  },
 ];
 
 function SS() {
-    const swiper22 = useSwiper();
-    console.log('swiper22', swiper22);
-    return (
-        <>
-            {[1, 2, 3].map((i, el) => {
-                return (
-                    <SwiperSlide key={el} style={{ color: 'red' }}>
-                        Slide {el}
-                    </SwiperSlide>
-                );
-            })}
-        </>
-    );
+  const swiper22 = useSwiper();
+  console.log("swiper22", swiper22);
+  return (
+    <>
+      {[1, 2, 3].map((i, el) => {
+        return (
+          <SwiperSlide key={el} style={{ color: "red" }}>
+            Slide {el}
+          </SwiperSlide>
+        );
+      })}
+    </>
+  );
 }
 
 function SwiperBody() {
-    const swiper2 = useSwiper();
-    console.log('swiper2', swiper2);
-    return (
+  const swiper2 = useSwiper();
+  console.log("swiper2", swiper2);
+  return (
     <div className="slideshow-container">
-        <SwiperSlide><SlideItem cardInfo={cardInfos[0]} /></SwiperSlide>
-        <SwiperSlide><SlideItem cardInfo={cardInfos[1]} /></SwiperSlide>
-        <SwiperSlide><SlideItem cardInfo={cardInfos[2]} /></SwiperSlide>
+      <SwiperSlide>
+        <SlideItem cardInfo={cardInfos[0]} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SlideItem cardInfo={cardInfos[1]} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SlideItem cardInfo={cardInfos[2]} />
+      </SwiperSlide>
 
-        <button onClick={() => swiper2.slidePrev()}>Prev</button>
-        <button onClick={() => swiper2.slideNext()}>Next</button>
+      <button onClick={() => swiper2.slidePrev()}>Prev</button>
+      <button onClick={() => swiper2.slideNext()}>Next</button>
     </div>
-    );
+  );
 }
 
-
 export function HoreseForSale() {
-    // const swiper2 = useSwiper();
-    // const swiper3 = useRef<SwiperRef>(null);
+  // const swiper2 = useSwiper();
+  // const swiper3 = useRef<SwiperRef>(null);
 
-    // console.log('swiper', swiper2, swiper3.current);
+  // console.log('swiper', swiper2, swiper3.current);
 
-    return (
-        <main id="events" className="page-events">
-            <Hero />
+  return (
+    <>
+      <main id="events" className="page-events">
+        <Hero />
 
-            {/* <Swiper ref={swiper3} modules={[Navigation, EffectCoverflow]} effect="fade">
+        {/* <Swiper ref={swiper3} modules={[Navigation, EffectCoverflow]} effect="fade">
                 <SS />
             </Swiper> */}
 
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                modules={[Navigation, EffectCoverflow]}
-                // onSlideChange={() => console.log('onSlideChange: slide change')}
-                // onSwiper={(swiper) => console.log('onSwiper', swiper)}
-            >
-                <SwiperBody />
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          modules={[Navigation, EffectCoverflow]}
+          // onSlideChange={() => console.log('onSlideChange: slide change')}
+          // onSwiper={(swiper) => console.log('onSwiper', swiper)}
+        >
+          <SwiperBody />
+        </Swiper>
 
-            </Swiper>
-
-
-            {/* <Swiper
+        {/* <Swiper
                 spaceBetween={50}
                 slidesPerView={3}
                 //modules={[Navigation, EffectCoverflow]}
@@ -130,7 +133,7 @@ export function HoreseForSale() {
 
             </Swiper> */}
 
-            {/* <section>
+        {/* <section>
                 <div className="slideshow-container">
 
                     <Slide1 />
@@ -229,55 +232,59 @@ description
 ----------------------------------------------
 */
 
-function SlideItem({ cardInfo }: { cardInfo: CardInfo; }) {
-    return (
-        <div className="mySlides fade">
-            <div className="event-card-top">
-                <img className="event-card-img" src={cardInfo.img} />
-            </div>
-            <div className="event-card-bottom">
+function SlideItem({ cardInfo }: { cardInfo: CardInfo }) {
+  return (
+    <div className="mySlides fade">
+      <div className="event-card-top">
+        <img className="event-card-img" src={cardInfo.img} />
+      </div>
+      <div className="event-card-bottom">
+        {cardInfo.cardDataItems.map((item, index) => (
+          <Fragment key={index}>
+            <p className="title-small">{item.title}</p>
+            <h2 className="lowercase">{item.description}</h2>
+          </Fragment>
+        ))}
 
-                {cardInfo.cardDataItems.map((item, index) => (
-                    <Fragment key={index}>
-                        <p className="title-small">{item.title}</p>
-                        <h2 className="lowercase">{item.description}</h2>
-                    </Fragment>
-                ))}
-
-                <p>{cardInfo.last}</p>
-                <p className="read-more">
-                    <a className="line" href={cardInfo.lastUrl}>
-                        Inquire about this horse
-                    </a>
-                </p>
-            </div>
-        </div>
-    );
+        <p>{cardInfo.last}</p>
+        <p className="read-more">
+          <a className="line" href={cardInfo.lastUrl}>
+            Inquire about this horse
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 function Slide1() {
-    return (<div className="mySlides fade">
-        <div className="event-card-top">
-            <img className="event-card-img" src="./src/assets/img/maggie-horse.jpg" />
-            {/*<div class="event-card-date">
+  return (
+    <div className="mySlides fade">
+      <div className="event-card-top">
+        <img
+          className="event-card-img"
+          src="./src/assets/img/maggie-horse.jpg"
+        />
+        {/*<div class="event-card-date">
             <p>ongoing</p>
         </div> */}
-        </div>
-        <div className="event-card-bottom">
-            <p className="title-small">Name</p>
-            <h2 className="lowercase">Seabiscuit</h2>
-            <p className="title-small">Breed</p>
-            <h2 className="lowercase">Male</h2>
-            <p className="title-small">Price</p>
-            <h2 className="lowercase">10K</h2>
-            <p>Buy Horse now yada yada yada</p>
-            <p className="read-more">
-                <a className="line" href="./voting.html">
-                    Inquire about this horse
-                </a>
-            </p>
-        </div>
-    </div>);
+      </div>
+      <div className="event-card-bottom">
+        <p className="title-small">Name</p>
+        <h2 className="lowercase">Seabiscuit</h2>
+        <p className="title-small">Breed</p>
+        <h2 className="lowercase">Male</h2>
+        <p className="title-small">Price</p>
+        <h2 className="lowercase">10K</h2>
+        <p>Buy Horse now yada yada yada</p>
+        <p className="read-more">
+          <a className="line" href="./voting.html">
+            Inquire about this horse
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 function Slide2() {
@@ -306,23 +313,23 @@ function Slide2() {
 }
 
 function Slide3() {
-    return (
-        <div className="mySlides fade">
-            <div className="event-card-top">
-                <img className="event-card-img" src="./src/assets/img/facility-1.jpg" />
-            </div>
+  return (
+    <div className="mySlides fade">
+      <div className="event-card-top">
+        <img className="event-card-img" src="./src/assets/img/facility-1.jpg" />
+      </div>
 
-            <div className="event-card-bottom">
-                <p className="title-small">Name</p>
-                <h2 className="lowercase">Oceanwafer</h2>
-                <p className="title-small">Breed</p>
-                <h2 className="lowercase">Male</h2>
-                <p className="title-small">Price</p>
-                <h2 className="lowercase">20K</h2>
-                <p>Buy Horse now yada yada yada</p>
-            </div>
+      <div className="event-card-bottom">
+        <p className="title-small">Name</p>
+        <h2 className="lowercase">Oceanwafer</h2>
+        <p className="title-small">Breed</p>
+        <h2 className="lowercase">Male</h2>
+        <p className="title-small">Price</p>
+        <h2 className="lowercase">20K</h2>
+        <p>Buy Horse now yada yada yada</p>
+      </div>
 
-            {/* <div className="event-card-bottom">
+      {/* <div className="event-card-bottom">
             <p className="title-small">Vote on the</p>
             <h2 className="lowercase">Next Meetup</h2>
             <p>
@@ -339,6 +346,6 @@ function Slide3() {
                 </a>
             </p>
         </div> */}
-        </div>
-    );
+    </div>
+  );
 }
